b273dfd18372a4be29b6eeb7cf8876f71f4c4e9e
Dominic Di'Mico <dominic.dimico@gmail.com>
2019-01-23 17:50:20 -0500

    Made Smartlog class

--- smartlog.py
+++ smartlog.py
@@ -1,165 +1,182 @@
-from blessings import Terminal
-from sys       import stdin
+from   blessings import Terminal
+from   sys       import stdin
 import os
 
 
-t = Terminal()
+class Smartlog():
+
+    t  = None;
+    fd = None;
+
+    def __init__(self, filename="/dev/stdout"):
+        try: self.fd = open(filename, "w+");
+        except: print "File Exception"
+        self.t = Terminal(stream=self.fd, force_styling=True);
+
+    # Print an alert message.
+    def alert(self, msg):
+        self.fd.write(self.t.red("*")), 
+        self.fd.write(self.t.bold("Alert:")),
+        self.fd.write(" %s!" % msg),
+        self.rok()
 
 
-# Print an alert message.
-def alert(msg):
-    print t.red("*"), 
-    print t.bold("Alert:"),
-    print "%s!" % msg,
-    rok()
+    # Print an alert message.
+    def prompt(self, msg):
+        self.fd.write(self.t.magenta("*")), 
+        self.fd.write(self.t.bold(" %s: " % msg)),
+        ret = stdin.readline();
+        return reself.t.rstrip().lstrip();
+
+    # Print a WARN message.
+    def warn(self, msg):
+        self.fd.write(self.t.yellow("*")), 
+        self.fd.write(self.t.bold("Warning:")),
+        self.fd.write(" %s." % msg),
+        self.warnok()
 
 
-# Print an alert message.
-def prompt(msg):
-    print t.magenta("*"), 
-    print t.bold("%s: " % msg),
-    ret = stdin.readline();
-    return ret.rstrip().lstrip();
-
-# Print a WARN message.
-def warn(msg):
-    print t.yellow("*"), 
-    print t.bold("Warning:"),
-    print "%s." % msg,
-    warnok()
+    def optip(self, msg):
+        """
+        Give a tip that says:
+        
+        "You can set it with the %s flag."
+        """
+        self.fd.write(self.t.blue("*")), 
+        self.fd.write(" You can set it with the %s flag." % msg),
+        self.infook()
 
 
-def optip(msg):
-    """
-    Give a tip that says:
-    
-    "You can set it with the %s flag."
-    """
-    print t.blue("*"), 
-    print "You can set it with the %s flag." % msg,
-    infook()
+    # Print an INFO message.
+    def tip(self, msg):
+        self.fd.write(self.t.blue("*")), 
+        self.fd.write(" %s." % msg),
+        self.infook()
 
 
-# Print an INFO message.
-def tip(msg):
-    print t.blue("*"), 
-    print "%s." % msg,
-    infook()
+    # Print an log message, but no OK or FAIL box.
+    def log(self, msg):
+        self.fd.write(self.t.green("*")), 
+        self.fd.write(" %s..." % msg),
+
+    # Print an log message, with OK.
+    def logok(self, msg):
+        self.log(msg);
+        self.ok();
+
+    # Print an log message, with no OK and newline.
+    def lognok(self, msg):
+        self.log(msg);
+        self.fd.write("\n");
+
+    # Print an OK box.
+    def ok(self):
+        with self.t.location(x=self.t.width-10):
+          self.fd.write("[  "),
+          self.fd.write(self.t.green("OK")),
+          self.fd.write("  ]"),
+        self.fd.write("\n")
+        #self.fd.write(str(self.t.width)+"\n")
 
 
-# Print an log message, but no OK or FAIL box.
-def log(msg):
-    print t.green("*"), 
-    print "%s..." % msg,
+    # Print a FAIL box.
+    def fail(self):
+        with self.t.location(x=self.t.width-10):
+          self.fd.write("[ "),
+          self.fd.write(self.t.red("FAIL")),
+          self.fd.write(" ]"),
+        self.fd.write("\n")
 
 
-# Print an OK box.
-def ok():
-    with t.location(x=t.width-10):
-      print "[ ",
-      print t.green("OK"),
-      print " ]",
-    print ""
+    # Print a yellow OK box.
+    def yok(self):
+        with self.t.location(x=self.t.width-10):
+          self.fd.write("[  "),
+          self.fd.write(self.t.yellow("OK")),
+          self.fd.write("  ]"),
+        self.fd.write("\n")
 
 
-# Print a FAIL box.
-def fail():
-    with t.location(x=t.width-10):
-      print "[",
-      print t.red("FAIL"),
-      print "]",
-    print ""
+    # Print a red OK box.
+    def rok(self):
+        with self.t.location(x=self.t.width-10):
+          self.fd.write("[  "),
+          self.fd.write(self.t.red("OK")),
+          self.fd.write("  ]"),
+        self.fd.write("\n")
 
 
-# Print a yellow OK box.
-def yok():
-    with t.location(x=t.width-10):
-      print "[ ",
-      print t.yellow("OK"),
-      print " ]",
-    print ""
+    # Print a WARN box.
+    def warnok(self):
+        with self.t.location(x=self.t.width-10):
+          self.fd.write("[ "),
+          self.fd.write(self.t.yellow("WARN")),
+          self.fd.write(" ]"),
+        self.fd.write("\n")
 
 
-# Print a red OK box.
-def rok():
-    with t.location(x=t.width-10):
-      print "[ ",
-      print t.red("OK"),
-      print " ]",
-    print ""
+    # Print an INFO box.
+    def infook(self):
+        with self.t.location(x=self.t.width-10):
+          self.fd.write("[ "),
+          self.fd.write(self.t.blue("INFO")),
+          self.fd.write(" ]"),
+        self.fd.write("\n")
 
 
-# Print a WARN box.
-def warnok():
-    with t.location(x=t.width-10):
-      print "[",
-      print t.yellow("WARN"),
-      print "]",
-    print ""
+    # Determine if program exists.
+    def which(self, program):
+        str = ' '.join(["Checking location of",program])
+        self.log(str)
+        def is_exe(self, fpath):
+            return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+        fpath, fname = os.path.split(program)
+        if fpath:
+            if is_exe(program):
+                self.ok()
+                return program
+        else:
+            for path in os.environ["PATH"].split(os.pathsep):
+                path = path.strip('"')
+                exe_file = os.path.join(path, program)
+                if is_exe(exe_file):
+                    self.ok()
+                    return exe_file
+        self.fail()
+        return None
 
 
-# Print an INFO box.
-def infook():
-    with t.location(x=t.width-10):
-      print "[",
-      print t.blue("INFO"),
-      print "]",
-    print ""
+    # Check if directory exists.
+    def check_dir(self, name):
+        str = ' '.join(["Checking if directory",name,"exists"])
+        self.log(str)
+        if os.path.isdir(name):
+            self.ok()
+            return True
+        else:
+            self.fail()
+            return False
 
 
-# Determine if program exists.
-def which(program):
-    str = ' '.join(["Checking location of",program])
-    log(str)
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            ok()
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            path = path.strip('"')
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                ok()
-                return exe_file
-    fail()
-    return None
+    # Check if file exists.
+    def check_file(self, name):
+        str = ' '.join(["Checking if file",name,"exists"])
+        self.log(str)
+        if os.path.isfile(name):
+            self.ok()
+            return True
+        else:
+            self.fail()
+            return False
 
 
-# Check if directory exists.
-def check_dir(name):
-    str = ' '.join(["Checking if directory",name,"exists"])
-    log(str)
-    if os.path.isdir(name):
-        ok()
-        return True
-    else:
-        fail()
-        return False
-
-
-# Check if file exists.
-def check_file(name):
-    str = ' '.join(["Checking if file",name,"exists"])
-    log(str)
-    if os.path.isfile(name):
-        ok()
-        return True
-    else:
-        fail()
-        return False
-
-
-# Check if environment variable is set.
-def check_var(name):
-    val = os.getkey(name)
-    str = ' '.join(["Checking if variable",name,"is set"])
-    log(str)
-    if val:
-        ok()
-    else:
-        fail()
+    # Check if environment variable is set
+    def check_var(self, name):
+        val = os.getkey(name)
+        str = ' '.join(["Checking if variable",name,"is set"])
+        self.log(str)
+        if val:
+            self.ok()
+        else:
+            self.fail()
 
---
+++ test.py
@@ -0,0 +1,10 @@
+import smartlog
+
+term = smartlog.Smartlog();
+
+term.log("Hey");
+term.ok();
+
+term.lognok("Hey");
+
+term.logok("Hey");
